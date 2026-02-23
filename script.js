b83b23cff6bfc9ea941de8424310b2b1dd04d4ff
Samad <Samad.o@outlook.com>
2026-02-23 13:28:30 -0500

    simple rock paper scissors complete

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let selection = ["🪨", "📃", "✂️"];
 
 const shotSelection = document.getElementById("shot-selection");
 
-document.addEventListener("click", () => {
+document.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("button")) return;
   let randSelection = selection[Math.floor(Math.random() * selection.length)];
 
   shotSelection.innerText = randSelection;
