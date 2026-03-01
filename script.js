b83f8f29bb2fdb65544ded27c1ea5511aa077154
Samad <Samad.o@outlook.com>
2026-03-01 16:48:55 -0500

    add user and bot selection displays to the game interface

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ let selection = ["🪨", "📃", "✂️"];
 
 const shotSelection = document.getElementById("shot-selection");
 
+const userShot = document.getElementById("user-selection");
+
 document.addEventListener("click", (e) => {
   if (!e.target.classList.contains("button")) return;
   let randSelection = selection[Math.floor(Math.random() * selection.length)];
 
   shotSelection.innerText = randSelection;
+  userShot.innerText = e.target.innerText;
 });
